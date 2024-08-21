573f9d139811569f7380aa7f7ed53206c06ea7dc
Mikael Gabryel Flor de Lis <gabryel.flordelis@gmail.com>
2024-08-21 19:04:14 +0800

    bug fix

--- main.py
+++ main.py
@@ -2,16 +2,25 @@ import RPi.GPIO as GPIO
 import time
 
 ena = 18
+
+in1 = 15
+in2 = 13
+in3 = 31
+in4 = 29
+
 enb = 16
 
-input = [15, 13, 31, 29]
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(ena, GPIO.OUT)
-GPIO.setup(enb, GPIO.OUT)
+input = [in1, in2, in3, in4]
 
-for i in input:
-    GPIO.setup(i, GPIO.OUT)
-    GPIO.output(i, GPIO.LOW)
+def main():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(ena, GPIO.OUT)
+    GPIO.setup(enb, GPIO.OUT)
 
+    for i in input:
+        GPIO.setup(i, GPIO.OUT)
+        GPIO.output(i, GPIO.LOW)
 
+if __name__ == "__main__":
+    main()
